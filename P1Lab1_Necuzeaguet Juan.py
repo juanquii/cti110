b8b5ea3ef5225aa0986cc519c34c21c22248ec7f
juanquicyb <necuzeaj3748@student.faytechcc.edu>
2025-06-11 12:19:21 -0400

    Add files via upload

---
+++ P1Lab1_Necuzeaguet Juan.py
@@ -0,0 +1,10 @@
+# Jane Smith
+# June 9, 2025
+# P1Lab1 - Introduction
+# This program gets a user's name and prints a welcome message.
+
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
